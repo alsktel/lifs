7b9d0116b55760e55031ec6f3193086f32ea2be2
eastev <eastevrud31@gmail.com>
2020-09-16 19:22:20 +0200

    Created free sector finder

--- include/lifs_commons.h
+++ include/lifs_commons.h
@@ -6,8 +6,17 @@
 #ifndef __LIFS_COMMONS_H__
 #define __LIFS_COMMONS_H__
 
+#include <stdint.h>
+
+#include <lifs_bitmap.h>
+
 // Common defines for LIFS
 #define _LIFS_SECTOR_SIZE_ 512
 #define _LIFS_MIN_FS_SIZE_ 7
+#define _LIFS_NAMES_LEN_ 256
+
+// Finds first free sector
+// Returns sector id, or 0 in case of error
+uint32_t find_first_free_sector(lifs_bitmap_t* bitmap);
 
 #endif
---
+++ src/commons.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+// Created by eastev <https://github.com/eastev>
+
+// This file is a part of LIFS creation tool
+
+#include <lifs_commons.h>
+
+uint32_t find_first_free_sector(lifs_bitmap_t* bitmap)
+{
+    uint32_t i = 0;
+
+    while(bitmap->bytes[i] == UINT8_MAX)
+    {
+        if(i > bitmap->size)
+        {
+            return 0;
+        }
+
+        i++;
+    }
+
+    uint8_t j = 0; 
+
+    while(bitmap->bytes[i] & (1 << j))
+    {
+        j++;
+    }
+
+    return i * _LIFS_BITMAP_SECTORS_PER_BYTE_ + j;
+}
